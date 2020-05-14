ee00d328547f67689d8c1960fa888c96f272a58d
Linus Jäderlund <linus.jaderlund@hjart-lungfonden.se>
2020-05-14 11:41:20 +0200

    Added support for .key file

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const decipher = require("./decipher");
-const [, , file, secret] = process.argv;
+const file = process.argv[2];
+const secretFile = ".key";
 
 const init = () => {
+  let secret = process.argv[3];
+
   if (!fs.existsSync(file)) {
     return console.log("File path passed to function does not exist");
   }
 
   if (!secret) {
-    return console.log("Must pass secret as second parameter to function");
+    if (!fs.existsSync(secretFile)) {
+      return console.log(
+        "Must pass secret as second parameter to function or create .key file"
+      );
+    }
+
+    secret = fs.readFileSync(secretFile, "utf8");
   }
 
   decipher(file, secret);
